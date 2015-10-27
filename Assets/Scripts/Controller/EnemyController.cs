1be69e131717229f4a1822debffb83fadede673a
Dani <daniandia@gmail.com>
2015-10-27 14:43:55 +0100

    prueba casas

---
+++ Assets/Scripts/Controller/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	private GameObject[] enemyPool;
+	private int 	   selectedEnemy;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InitEnemyPool(){
+		enemyPool = GameObject.FindGameObjectsWithTag("Enemy");
+		//foreach (GameObject _obj in GameObject.FindGameObjectsWithTag("Enemy")) {}
+	}
+}
