61fce6729e2ac8133d30211b3c125e5c87c637da
Alfonso Nava Morales <alfonsonavamorales@gmail.com>
2015-11-02 19:28:43 +0100

    MissionHelper
    No se si la he liado con un prefab...

---
+++ Assets/Scripts/Utils/LoadLevelDialog.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+
+public class LoadLevelDialog : MonoBehaviour {
+
+	public string level;
+	public JsonData dataFile;
+
+	// Use this for initialization
+	void Start () {
+		dataFile = loadJSON.Instance.loadFile (level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
